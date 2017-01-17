79ce3526af57770e281930311b100de680508e32
lgkinter <lgkinter@yahoo.com>
2017-01-16 21:20:16 -0500

    add comments feature

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170116171620) do
+ActiveRecord::Schema.define(version: 20170116214459) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -19,6 +19,15 @@ ActiveRecord::Schema.define(version: 20170116171620) do
     t.datetime "updated_at", null: false
     t.string   "hover_text"
     t.string   "image"
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "author_name"
+    t.text     "body"
+    t.integer  "article_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.index ["article_id"], name: "index_comments_on_article_id"
   end
 
 end
