f84b04de3d479315a34ca9d183e1e4f0b5745197
lgkinter <lgkinter@yahoo.com>
2017-01-18 18:58:08 -0500

    integrate design

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,10 @@
+class StaticPagesController < ApplicationController
+  def about
+  end
+
+  def books
+  end
+
+  def tools
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
+  get 'static_pages/about'
+
+  get 'static_pages/books'
+
+  get 'static_pages/tools'
+
   root to: "articles#index"
-  resources :articles do
+  #get '/about' to 'static_pages#about'
+  #get '/books' to 'static_pages#books'
+  #get '/tools' to 'static_pages#tools'
+  resources :articles, path: :pixels do
     resources :comments
   end
   resources :tags
