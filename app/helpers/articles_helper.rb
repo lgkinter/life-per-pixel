61ce7aa107ac15ccbc71b922623c0c8cdadbcaa8
lgkinter <lgkinter@yahoo.com>
2017-01-18 11:25:06 -0500

    add tagging feature

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,7 +1,7 @@
 module ArticlesHelper
 
   def article_params
-    params.require(:article).permit(:title, :hover_text, :image, :body)
+    params.require(:article).permit(:title, :hover_text, :image, :body, :tag_list)
   end
 
 end
